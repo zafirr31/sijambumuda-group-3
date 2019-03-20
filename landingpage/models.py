8e4e7fa350c2d63fd8425b43080c21bb96af266d
Zafirrt <zafir.r.taufik@gmail.com>
2019-03-20 21:47:03 +0700

    Show Data Peminjaman selesai

--- landingpage/models.py
+++ landingpage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Peminjaman(models.Model):
+
+	book_title = models.CharField(
+			max_length=100,
+		)
+	borrower_name = models.CharField(
+			max_length=100,
+		)
+	borrow_time = models.DateField()
+    

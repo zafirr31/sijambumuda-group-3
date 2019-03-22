3b3ab9070aa7422af29ec09a63280b55b3e7f8b2
Shafiya Adzhani <adz.arsym@gmail.com>
2019-03-22 09:43:12 +0700

    added css

--- form_anggota/forms.py
+++ form_anggota/forms.py
@@ -2,6 +2,42 @@ from django import forms
 from .models import Member
 
 class DaftarMember(forms.ModelForm):
+    Nama = forms.CharField(widget=forms.TextInput(
+        attrs = {
+            "class" : "form-control",
+                }
+    ))
+
+    Nomor_Identitas = forms.CharField(widget=forms.TextInput(
+        attrs = {
+            "class" : "form-control",
+                }
+    ))
+
+    Username = forms.CharField(widget=forms.TextInput(
+        attrs = {
+            "class" : "form-control",
+                }
+    ))
+
+    Email = forms.CharField(widget=forms.TextInput(
+        attrs = {
+            "class" : "form-control",
+                }
+    ))
+
+    Password = forms.CharField(widget=forms.PasswordInput(
+        attrs = {
+            "class" : "form-control",
+                }
+    ))
+
+    Alamat_Rumah = forms.CharField(widget=forms.TextInput(
+        attrs = {
+            "class" : "form-control",
+                }
+    ))
+
     class Meta:
         model = Member
         fields = ('Nama', 'Nomor_Identitas',
--- shafiya_pinjam/forms.py
+++ shafiya_pinjam/forms.py
@@ -4,21 +4,21 @@ from form_anggota.models import Member
 
 class PinjamForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class':'form',
+        'class':'form-control',
         'required':'True',
         'placeholder':'Username Anda',
     }))
     email = forms.EmailField(widget=forms.TextInput(attrs={
-        'class':'form',
+        'class':'form-control',
         'required':'True',
         'placeholder':'Email Anda',
     }))
     nomor_buku = forms.CharField(widget=forms.TextInput(attrs={
-        'class':'form',
+        'class':'form-control',
         'required':'True',
     }))
     tanggal_pinjam= forms.DateField(widget=forms.TextInput(attrs={
-        'class':'form',
+        'class':'form-control',
         'required':'True',
         'placeholder':'Tanggal Pinjam?',
     }))

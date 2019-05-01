856772d02c14609207ca1f422b4f084c8175322f
Shafiya Adzhani <adz.arsym@gmail.com>
2019-05-01 17:41:58 +0700

    tried to add session

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 
+
 def index(request):
-	if request.session.has_key('username'):
-		user = request.session['username']
-	else:
-		user = ""
-	index_context = {'user': user}
-	return render(request, "index.html", index_context)
+    if request.session.has_key('username'):
+        user = request.session['username']
+    else:
+        user = ""
+    index_context = {'user': user}
+    return render(request, "index.html", index_context)
--- shafiya_pinjam/views.py
+++ shafiya_pinjam/views.py
@@ -8,21 +8,46 @@ import datetime
 
 # Create your views here.
 
-def pinjam(request):
-    if request.method == "POST":
-        pinjam_form = PinjamForm(request.POST)
-        if pinjam_form.is_valid():
-            pinjam_model = PinjamModel(
-                username=pinjam_form.cleaned_data['username'],
-                email=pinjam_form.cleaned_data['email'], 
-                nomor_buku=pinjam_form.cleaned_data['nomor_buku'], 
-                tanggal_pinjam=datetime.datetime.now(),
-                nama_peminjam = Member.objects.filter(username=pinjam_form.cleaned_data['username']).values()[0]["username"],
-                buku_dipinjam = Buku.objects.filter(nomor_buku=pinjam_form.cleaned_data['nomor_buku']).values()[0]["judul_buku"],
-            )
-            pinjam_model.save()
-            return HttpResponseRedirect('/datapeminjaman/')
-    else:        
-        pinjam_form = PinjamForm()
 
+def pinjam(request):
+    if request.user.is_authenticated:
+        request.session['username'] = request.user.username
+        request.session['email'] = request.user.email
+
+        response.form['form'] = PinjamForm()
+
+        if request.method == "POST":
+            pinjam_form = PinjamForm(request.POST)
+            if pinjam_form.is_valid():
+                response['username'] = request.session['username']
+                response['email'] = request.session['email']
+                response['nomor_buku'] = request.POST['nomor_buku']
+                response['tanggal_pinjam'] = request.POST['tanggal_pinjam']
+
+                pinjam_model = PinjamModel(name=response['name'], email=response['email'], nomor_buku=response['nomor_buku'],
+                                           tanggal_pinjam=response['tanggal_pinjam'])
+                pinjam_model.save()
+                return HttpResponseRedirect('/')
+            else:
+                pinjam_form = PinjamForm()
+
+                # if request.method == "POST":
+                #     pinjam_form = PinjamForm(request.POST)
+                #     if pinjam_form.is_valid():
+                #         pinjam_model = PinjamModel(
+                #             username=pinjam_form.cleaned_data['username'],
+                #             email=pinjam_form.cleaned_data['email'],
+                #             nomor_buku=pinjam_form.cleaned_data['nomor_buku'],
+                #             tanggal_pinjam=datetime.datetime.now(),
+                #             nama_peminjam=Member.objects.filter(
+                #                 username=pinjam_form.cleaned_data['username']).values()[0]["username"],
+                #             buku_dipinjam=Buku.objects.filter(
+                #                 nomor_buku=pinjam_form.cleaned_data['nomor_buku']).values()[0]["judul_buku"],
+                #         )
+                #         pinjam_model.save()
+                #         return HttpResponseRedirect('/datapeminjaman/')
+                # else:
+                #     pinjam_form = PinjamForm()
+    else:
+        return HttpResponseRedirect('/login/')
     return render(request, 'page/form-pinjam.html', {'form': pinjam_form})

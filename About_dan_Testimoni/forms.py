2118d07d60183b0f2a6abd97260a01c5fa45a9a6
Rendya Yuschak <rendyayuschak@gmail.com>
2019-05-02 16:18:12 +0700

    menambahi teks

---
+++ About_dan_Testimoni/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Testimoni
+
+
+class IsiTestimoni(forms.ModelForm):
+    class Meta:
+        model = Testimoni
+        fields = ('Username','Pesan')
--- About_dan_Testimoni/views.py
+++ About_dan_Testimoni/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 import datetime
+from .models import Testimoni
+from .forms import IsiTestimoni
 
 def about(request):
-    return render(request,'about_dan_testimoni.html')
+    if request.method == 'POST':
+        formTestimoni = IsiTestimoni(request.POST)
+        post = formTestimoni.save(commit=False)
+        post.save()
+        isi = Testimoni.objects.all()
+        return render(request, 'about_dan_testimoni.html', {'form' : formTestimoni, 'isi' : isi})
+    else:
+        formTestimoni = IsiTestimoni()
+        isi = " "
+    return render(request, 'about_dan_testimoni.html', {'form' : formTestimoni})
+
+
+    #return render(request,'about_dan_testimoni.html')
 
 
 # Create your views here.

51615566ce8d45c6b36ffa2aaa033c418d7ac6a0
jahns_michael <michael.jf.jm@gmail.com>
2019-05-06 18:48:34 +0700

    update

--- history_pinjaman/views.py
+++ history_pinjaman/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 from shafiya_pinjam.models import PinjamModel
 from show_buku.models import Buku
-from form_anggota.models import Member
+from form_anggota.models import Member, Profile
+from .forms import ProfileForm
 from django.http import HttpResponseRedirect, JsonResponse
 
 # Create your views here.
 # @login_required
 
 def show_history(request):
-    return render(request, 'history.html')
+    if request.session.has_key("username"):             # if request.user.is_authenticated :
+        username = request.session["username"]          # username = request.user.username
+        member = Member.objects.get(username=username)
+        if Profile.objects.filter(member=member).exists():
+            profile = Profile.objects.get(member=member)    # profile = Profile.objects.get(member=request.user)
+            return render(request, 'history.html', {'profile':profile, 'username':username})
+        else :
+            return render(request, 'history.html')
+    return HttpResponseRedirect("/login/")
 
 def history_json(request):
-    if request.session.has_key("username"):
-        username = request.session["username"]
+    if request.session.has_key("username"):             # if request.user.is_authenticated :
+        username = request.session["username"]          # if request.user.username:
         db_pinjam = PinjamModel.objects.filter(username=username)
-        # db_buku = Buku.objects.all()
         response_data = []
         for pinjam in db_pinjam:
             attr = dict()
@@ -27,3 +35,22 @@ def history_json(request):
             response_data.append(attr)
         return JsonResponse(response_data, safe=False)
     return HttpResponseRedirect("/")
+
+def profile(request):
+    if request.session.has_key("username"):
+        if request.method == "POST" :
+            form = ProfileForm(request.POST, request.FILES)
+            if form.is_valid() :
+                member = Member.objects.get(username=request.session["username"])
+                propic = request.FILES['profile_picture']
+                address = form.cleaned_data["alamat_rumah"]
+                if Profile.objects.filter(member=member).exists() :
+                    query = Profile.objects.get(member=member)
+                    query.delete()
+                profile = Profile(member=member, profile_picture=propic, alamat_rumah=address)
+                profile.save()
+                return HttpResponseRedirect("/history/")
+        else :
+            form = ProfileForm()
+        return render(request, "proform.html", {"form": form})
+    return HttpResponseRedirect("/login/")

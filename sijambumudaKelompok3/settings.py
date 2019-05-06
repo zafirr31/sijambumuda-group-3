b94431b03e6ee4d94cab5f6fa7841eb6e3ebc2e8
Zafirrt <zafir.r.taufik@gmail.com>
2019-05-06 14:38:37 +0700

    pls work pls :(

--- sijambumudaKelompok3/settings.py
+++ sijambumudaKelompok3/settings.py
@@ -137,6 +137,17 @@ AUTHENTICATION_BACKENDS = (
 	'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'myapp.pipeline.load_user',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 

354f46e33f08543d10a5eae5acdc8344f03d88f3
Rendya Yuschak <rendyayuschak@gmail.com>
2019-05-07 16:47:09 +0700

    membetulkan cleaned data

--- About_dan_Testimoni/views.py
+++ About_dan_Testimoni/views.py
@@ -4,27 +4,22 @@ from .models import Testimoni
 from .forms import IsiTestimoni
 
 def about(request):
-    #if request.session.has_key('username'):
-    if request.method == 'POST':
-        formTestimoni = IsiTestimoni(request.POST)
-        if formTestimoni.is_valid():
-            model_testimoni = Testimoni(
-            Pesan = IsiTestimoni.cleaned_data['Pesan'],
-            Tanggal_Pesan = datetime.datetime.now()
-            )
-            model_testimoni.save()
-        isi = Testimoni.objects.all()
-        return HttpResponseRedirect('/about-dan-testimoni/')
-        #return render(request, 'about_dan_testimoni.html', {'form' : formTestimoni, 'isi' : isi})
-    else:
-        formTestimoni = IsiTestimoni()
-        isi = " "
-        return render(request, 'about_dan_testimoni.html', {'form' : formTestimoni, 'isi' : isi})
+    if request.session.has_key('username'):
+        if request.method == 'POST':
+            formTestimoni = IsiTestimoni(request.POST)
+            if formTestimoni.is_valid():
+                model_testimoni = Testimoni(
+                Username = request.session['username'],
+                Pesan = formTestimoni.cleaned_data['Pesan'],
+                Tanggal_Pesan = datetime.datetime.now()
+                )
+                model_testimoni.save()
+            isi = Testimoni.objects.all()
+            return HttpResponseRedirect('/about-dan-testimoni/')
 
-    #isi = Testimoni.objects.all()
-    #return render(request, 'about_dan_testimoni.html', {'isi' : isi})
+            formTestimoni = IsiTestimoni()
+            isi = Testimoni.objects.all()
+            return render(request, 'about_dan_testimoni.html', {'form' : formTestimoni, 'isi' : isi})
 
-    #return render(request,'about_dan_testimoni.html')
-
-
-# Create your views here.
+    isi = Testimoni.objects.all()
+    return render(request, 'about_dan_testimoni.html', {'isi' : isi})

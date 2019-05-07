323437f962ade89772a5874aed7f126c08e468a8
Shafiya Adzhani <adz.arsym@gmail.com>
2019-05-07 13:58:06 +0700

    resolve again

--- history_pinjaman/urls.py
+++ history_pinjaman/urls.py
@@ -1,7 +1,8 @@
-from .views import show_history, history_json
+from .views import show_history, history_json, profile
 from django.urls import path
 
 urlpatterns = [
     path('', show_history, name='show_history'),
-    path('json/', history_json, name='history_json')
+    path('json/', history_json, name='history_json'),
+    path('profile/', profile, name='profile')
 ]

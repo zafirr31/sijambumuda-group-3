ab36c31088fdaf3d4a04d48485acedcddb8328a0
Shafiya Adzhani <adz.arsym@gmail.com>
2019-05-07 16:28:05 +0700

    finally done

--- history_pinjaman/forms.py
+++ history_pinjaman/forms.py
@@ -1,12 +1,9 @@
 from django import forms
 from form_anggota.models import Profile
 
+
 class ProfileForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-<<<<<<< HEAD
         fields = ['profile_picture', 'alamat_rumah']
-=======
-        fields = ['profile_picture', 'alamat_rumah']
->>>>>>> c70cf14e65b40dae370759ed8db67a124dedf792

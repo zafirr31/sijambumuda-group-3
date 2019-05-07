2d51f629d956732c400c476d57b14e76701b46b9
Shafiya Adzhani <adz.arsym@gmail.com>
2019-05-07 16:19:34 +0700

    hmm again try to fix

--- landingpage/views.py
+++ landingpage/views.py
@@ -3,18 +3,9 @@ from django.template import RequestContext
 
 
 def index(request):
-<<<<<<< HEAD
-    if request.session.has_key('username'):
-        user = request.session['username']
+    if request.user.is_authenticated:
+        user = request.user.username
     else:
         user = ""
     # index_context = {'user': user}
     return render(request, "index.html")
-=======
-	if request.user.is_authenticated :
-		user = request.user.username
-	else:
-		user = ""
-	# index_context = {'user': user}
-	return render(request, "index.html")
->>>>>>> c70cf14e65b40dae370759ed8db67a124dedf792

3f8b5be99151c7abd084a550c6557e893620f06a
Rendya Yuschak <rendyayuschak@gmail.com>
2019-05-07 20:59:41 +0700

    Fitur udah jadi cuma ajaxnya nggak jalan entah kenapa

---
+++ About_dan_Testimoni/static/tampilkantestimoni.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+  show();
+  // $('#submit').click(function() {
+  //     $.ajax({
+  //         url : "buat/",
+  //         type: "POST",
+  //         data : {
+  //             pesan:$("#id_Pesan").val(),
+  //             csrfmiddlewaretoken:$("input[name=csrfmiddlewaretoken]").val(),
+  //         },
+  //         success : function() {
+  //             $("#id_Pesan").val("");
+  //         }
+  //     });
+  //
+  // });
+});
+
+// $('#submit').click(function() {
+//     $.ajax({
+//         url : "buat/",
+//         type: "POST",
+//         data : {
+//             pesan:$("#id_Pesan").val(),
+//             csrfmiddlewaretoken:$("input[name=csrfmiddlewaretoken]").val(),
+//         },
+//         success : function() {
+//             $("#id_Pesan").val("");
+//         }
+//     });
+//
+// });
+
+function show() {
+  $.ajax({
+      url: "tampil/",
+      type: "GET",
+      dataType: "json",
+      success: function(data) {
+        var innerHTML = '';
+          for(var i = 0; i < data.length; i++){
+            username = '<h2>' + data[i].Username + '</h2>';
+            pesan = '<p>' + data[i].Pesan + '</p>';
+            tanggal_pesan = '<h4>' + data[i].Tanggal_Pesan + '</h4>';
+
+            innerHTML += username + pesan + tanggal_pesan;
+          }
+
+        $("#tampilkanTestimoni").html(innerHTML);
+      }
+  })
+}
+
+function buat() {
+  $.ajax({
+      url : "buat/",
+      type: "POST",
+      data : {
+          pesan:$("#id_Pesan").val(),
+          csrfmiddlewaretoken:$("input[name=csrfmiddlewaretoken]").val(),
+      },
+      success : function() {
+          $("#id_Pesan").val("");
+          alert("success")
+      }
+  });
+}

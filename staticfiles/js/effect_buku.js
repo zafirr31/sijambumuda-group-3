f0e22b08aa0713129b52392861ff355b34eebf0f
Shafiya Adzhani <adz.arsym@gmail.com>
2019-05-29 22:31:00 +0700

    final forget

--- staticfiles/js/effect_buku.js
+++ staticfiles/js/effect_buku.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     let currentrightborder = $(".border").css("border-right-color");
     let currentbottomborder = $(".border").css("border-bottom-color");
     let currentbutton = $(".border").css("background-color");
+    let currentswitchbutton = $("#switch").css("background-color");
+    let currentnamebutton = $("#name").css("background-color");
 
     let expectbg = "rgb(255, 108, 99)";
     let expecttopborder = "rgb(255, 108, 99)";
@@ -12,6 +14,8 @@ $(document).ready(function () {
     let expectrightborder = "rgb(255, 108, 99)";
     let expectbottomborder = "rgb(255, 108, 99)";
     let expectbutton = "rgb(255, 108, 99)";
+    let expectswitchbutton = "rgb(255, 108, 99)";
+    let expectnamebutton = "rgb(255, 108, 99)";
 
     // $(".loader")
     //     .delay(600)
@@ -24,6 +28,8 @@ $(document).ready(function () {
         $(".border").css("border-right-color", expectrightborder);
         $(".border").css("border-bottom-color", expectbottomborder);
         $(".btn-primary").css("background-color", expectbutton);
+        $("#switch").css("background-color", expectswitchbutton);
+        $("#name").css("background-color", expectnamebutton);
     });
 
     $("#switch").mouseleave(function () {
@@ -33,6 +39,8 @@ $(document).ready(function () {
         $(".border").css("border-right-color", currentrightborder);
         $(".border").css("border-bottom-color", currentbottomborder);
         $(".btn-primary").css("background-color", currentbutton);
+        $("#switch").css("background-color", currentswitchbutton);
+        $("#name").css("background-color", currentnamebutton);
     });
 
     $("#switch").click(function () {
@@ -42,6 +50,8 @@ $(document).ready(function () {
         $(".border").css("border-right-color", expectrightborder);
         $(".border").css("border-bottom-color", expectbottomborder);
         $(".btn-primary").css("background-color", expectbutton);
+        $("#switch").css("background-color", expectswitchbutton);
+        $("#name").css("background-color", expectnamebutton);
         if (expectbg == "rgb(255, 108, 99)") {
             expectbg = "rgb(108, 99, 255)";
             expecttopborder = "rgb(108, 99, 255)";
@@ -49,12 +59,16 @@ $(document).ready(function () {
             expectrightborder = "rgb(108, 99, 255)";
             expectbottomborder = "rgb(108, 99, 255)";
             expectbutton = "rgb(108, 99, 255)";
+            expectswitchbutton = "rgb(108, 99, 255)";
+            expectnamebutton = "rgb(108, 99, 255)";
             currentbg = "rgb(255, 108, 99)";
             currenttopborder = "rgb(255, 108, 99)";
             currentleftborder = "rgb(255, 108, 99)";
             currentrightborder = "rgb(255, 108, 99)";
-            currentbottomborder ="rgb(255, 108, 99)";
+            currentbottomborder = "rgb(255, 108, 99)";
             currentbutton = "rgb(255, 108, 99)";
+            currentswitchbutton = "rgb(255, 108, 99)";
+            currentnamebutton = "rgb(255, 108, 99)";
         } else {
             currentbg = "rgb(108, 99, 255)";
             currenttopborder = "rgb(108, 99, 255)";
@@ -62,12 +76,16 @@ $(document).ready(function () {
             currentrightborder = "rgb(108, 99, 255)";
             currentbottomborder = "rgb(108, 99, 255)";
             currentbutton = "rgb(108, 99, 255)";
+            currentswitchbutton = "rgb(108, 99, 255)";
+            currentnamebutton = "rgb(108, 99, 255)";
             expectbg = "rgb(255, 108, 99)";
             expecttopborder = "rgb(255, 108, 99)";
             expectleftborder = "rgb(255, 108, 99)";
             expectrightborder = "rgb(255, 108, 99)";
-            expectbottomborder ="rgb(255, 108, 99)";
+            expectbottomborder = "rgb(255, 108, 99)";
             expectbutton = "rgb(255, 108, 99)";
+            expectswitchbutton = "rgb(255, 108, 99)";
+            expectnamebutton = "rgb(255, 108, 99)";
         }
     });
 });
